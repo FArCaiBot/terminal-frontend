61453e27e980ceb31e968b86d919502a0eaae49e
FArCaiBot <flavio397david@gmail.com>
2024-06-15 15:53:46 -0500

    Fixed #1: changed to primereact

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { Index } from "./pages/public/index/Index";
+import { PrimeReactProvider } from "primereact/api";
+import { addLocaleEs } from "./config/locale";
 
 function App() {
-  return <Index />;
+  useEffect(() => {
+    addLocaleEs();
+  }, []);
+  return (
+    <PrimeReactProvider value={{ locale: "es", ripple: true }}>
+      <Index />;
+    </PrimeReactProvider>
+  );
 }
 
 export default App;
--- src/pages/public/index/Index.jsx
+++ src/pages/public/index/Index.jsx
@@ -1,10 +1,88 @@
 import { Navbar } from "../../../components/navbar/Navbar";
 import { Banner } from "../../../components/Banner/Banner";
+
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
 export const Index = () => {
+  const products = [
+    {
+      code: "P001",
+      name: "Laptop",
+      category: "Electronics",
+      quantity: 15,
+    },
+    {
+      code: "P002",
+      name: "Smartphone",
+      category: "Electronics",
+      quantity: 30,
+    },
+    {
+      code: "P003",
+      name: "Tablet",
+      category: "Electronics",
+      quantity: 20,
+    },
+    {
+      code: "P004",
+      name: "Headphones",
+      category: "Accessories",
+      quantity: 50,
+    },
+    {
+      code: "P005",
+      name: "Smartwatch",
+      category: "Wearables",
+      quantity: 25,
+    },
+    {
+      code: "P006",
+      name: "Bluetooth Speaker",
+      category: "Accessories",
+      quantity: 40,
+    },
+    {
+      code: "P007",
+      name: "Monitor",
+      category: "Electronics",
+      quantity: 10,
+    },
+    {
+      code: "P008",
+      name: "Keyboard",
+      category: "Accessories",
+      quantity: 35,
+    },
+    {
+      code: "P009",
+      name: "Mouse",
+      category: "Accessories",
+      quantity: 45,
+    },
+    {
+      code: "P010",
+      name: "External Hard Drive",
+      category: "Storage",
+      quantity: 20,
+    },
+  ];
+
   return (
     <>
       <Navbar />
       <Banner />
+      <Button outlined label="Hola" />
+      <DataTable
+        value={products}
+        tableStyle={{ minWidth: "50rem" }}
+        size="small"
+      >
+        <Column filter field="code" header="Code"></Column>
+        <Column field="name" header="Name"></Column>
+        <Column field="category" header="Category"></Column>
+        <Column field="quantity" header="Quantity"></Column>
+      </DataTable>
     </>
   );
 };
